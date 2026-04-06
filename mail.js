7e0f23ea6992a238623005923b5e23ef7bc17507
dharini21 <sridharini2103@gmail.com>
2026-04-06 12:38:44 +0530

    06-04-2026-smtp

--- mail.js
+++ mail.js
@@ -78,7 +78,9 @@ app.use(express.json());
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
